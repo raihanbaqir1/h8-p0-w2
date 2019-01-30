55a5aa54967b6ccec68ac9faf46e5b78087bbf57
Raihan <raihanbaqir@gmail.com>
2019-01-30 15:57:21 +0700

    add exercises5

--- exercises5.js
+++ exercises5.js
@@ -1,3 +1,5 @@
+//Let's form a sentence
+
 var word = 'Javascript';
 var second = 'is';
 var third = 'awesome';
@@ -7,3 +9,50 @@ var sixth = 'love';
 var seventh = 'it';
 
 console.log(word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh)
+
+//Index Accessing 1 by 1
+var word = 'wow Javascript is so cool';
+var firstWord = word[0]+word[1]+word[2];
+var secondWord = word[4]+word[5]+word[6]+word[7]+word[8]+word[9]+word[10]+word[11]+word[12]+word[13];
+var thirdWord = word[15]+word[16];
+var fourthWord = word[18]+word[19];
+var fifthWord = word[21]+word[22]+word[23]+word[24];
+
+console.log('First Word: '+firstWord);
+console.log('Second Word: '+secondWord);
+console.log('Third Word: '+thirdWord);
+console.log('Fourth Word: '+fourthWord);
+console.log('Fifth Word: '+fifthWord);
+
+//Breaking Sentence (Again) using Substring
+
+var word3 = 'wow Javascript is so cool';
+var firstWord = word3.substring(0,3);
+var secondWord = word3.substring(4,14);
+var thirdWord = word3.substring(15,17);
+var fourthWord = word3.substring(18,20);
+var fifthWord = word3.substring(21,25);
+
+console.log('First Word: '+firstWord);
+console.log('Second Word '+secondWord);
+console.log('Third Word: '+thirdWord);
+console.log('Fourth Word: '+fourthWord);
+console.log('Fifth Word '+fifthWord);
+
+//Breaking Sentence (yet Again) and Count Each Length
+
+var word4 = 'wow Javascript is so cool';
+var firstWord4 = word4.substring(0,3);
+var secondWord4 = word4.substring(4,14);
+var thirdWord4 = word4.substring(15,17);
+var fourthWord4 = word4.substring(18,20);
+var fifthWord4 = word4.substring(21,25);
+
+firstWordLength = firstWord4.length;
+secondWordLength = secondWord4.length;
+thirdWordLength = thirdWord4.length;
+fourthWordLength = fourthWord4.length;
+fifthWordLength = fifthWord4.length;
+
+console.log('First Word '+firstWord4+', with length: '+firstWordLength);
+console.log('Second Word')
