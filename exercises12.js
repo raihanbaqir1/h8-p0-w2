b63dae917af3afdc1c1af396ef6b9db249ded0a2
Raihan <raihanbaqir@gmail.com>
2019-02-01 09:25:45 +0700

    change exercises12

--- exercises12.js
+++ exercises12.js
@@ -3,13 +3,13 @@
 
 function konversiMenit(menit){
     if(menit >=1 && menit <=9){
-        return '00:'+'0'+menit;
+        return '0:'+'0'+menit;
     } else if(menit >=10 && menit <=59){
-        return '00:'+menit;
+        return '0:'+menit;
     } else if(menit >=60 && menit%60 <10){
         return Math.floor((menit/60))+':'+'0'+(menit%60);
     } else if(menit >=60 && menit%60 >=10){
         return Math.floor((menit/60))+':'+(menit%60);
     }
 }
-console.log(konversiMenit(88));
+console.log(konversiMenit(53));
